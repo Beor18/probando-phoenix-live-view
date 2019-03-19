9487e1b975a1cd758f7a8e4cb020f67d9ac511f7
Fernando Lopez <fer.ragnar19@gmail.com>
2019-03-19 19:42:41 -0300

    modificado

--- lib/probandoPhoenixLiveView_web/live/github_deploy_view.ex
+++ lib/probandoPhoenixLiveView_web/live/github_deploy_view.ex
@@ -6,37 +6,37 @@ defmodule ProbandoPhoenixLiveViewWeb.GithubDeployView do
   end
 
   def mount(_session, socket) do
-    {:ok, assign(socket, deploy_step: "Ready!", status: "ready")}
+    {:ok, assign(socket, deploy_step: "Preparado para comenzar!", status: "ready")}
   end
 
   def handle_event("github_deploy", _value, socket) do
-    IO.puts "Starting deploy..."
+    IO.puts "Agregando archivos..."
     System.cmd("git", ["add", "."], into: IO.stream(:stdio, :line))
     Process.send_after(self(), :create_org, 1000)
-    {:noreply, assign(socket, deploy_step: "Starting deploy...", status: "deploy")}
+    {:noreply, assign(socket, deploy_step: "Agregando archivos...", status: "deploy")}
   end
 
   def handle_info(:create_org, socket) do
-    IO.puts "Creating org..."
+    IO.puts "Creando Commit..."
     System.cmd("git", ["commit", "-m", "modificado"], into: IO.stream(:stdio, :line))
     Process.send_after(self(), :create_repo, 1000)
-    {:noreply, assign(socket, deploy_step: "Creating GitHub org...", status: "create-org")}
+    {:noreply, assign(socket, deploy_step: "Creando Commit...", status: "create-org")}
   end
 
   def handle_info(:create_repo, socket) do
-    IO.puts "Creating repo..."
+    IO.puts "Creando repo"
     Process.send_after(self(), :push_contents, 1000)
-    {:noreply, assign(socket, deploy_step: "Creating GitHub repo...", status: "create-repo")}
+    {:noreply, assign(socket, deploy_step: "Creando repo", status: "create-repo")}
   end
 
   def handle_info(:push_contents, socket) do
-    IO.puts "Pushing contents..."
+    IO.puts "Push a Repo..."
     Process.send_after(self(), :done, 1000)
-    {:noreply, assign(socket, deploy_step: "Pushing to repo...", status: "push-contents")}
+    {:noreply, assign(socket, deploy_step: "Push a Repo...", status: "push-contents")}
   end
 
   def handle_info(:done, socket) do
-    IO.puts "Done!"
-    {:noreply, assign(socket, deploy_step: "Done!", status: "done")}
+    IO.puts "¡Listo!"
+    {:noreply, assign(socket, deploy_step: "¡Listo!", status: "done")}
   end
 end
