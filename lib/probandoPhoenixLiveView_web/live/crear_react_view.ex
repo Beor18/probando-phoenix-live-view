dd4dacc3249cd31378965952b2628170a024fbc6
Fernando Lopez <fer.ragnar19@gmail.com>
2019-03-22 09:21:07 -0300

    modificado

--- lib/probandoPhoenixLiveView_web/live/crear_react_view.ex
+++ lib/probandoPhoenixLiveView_web/live/crear_react_view.ex
@@ -6,19 +6,19 @@ defmodule ProbandoPhoenixLiveViewWeb.CrearReactView do
   end
 
   def mount(_session, socket) do
-    {:ok, assign(socket, crear_step: "Preparado para crear!", status: "listo")}
+    {:ok, assign(socket, crear_step: "Preparado para crear!", status: "ready")}
   end
 
   def handle_event("crear_app", _value, socket) do
     IO.puts "Creando archivos..."
-    System.cmd("create-react-app", ["demo-react"], into: IO.stream(:stdio, :line))
-    Process.send_after(self(), :verificar, 1000)
+    Process.send_after(self(), :verificar, 10000)
     {:noreply, assign(socket, crear_step: "Creando archivos...", status: "crear-app")}
   end
 
   def handle_info(:verificar, socket) do
     IO.puts "Verificando..."
-    Process.send_after(self(), :listo, 1000)
+    System.cmd("create-react-app", ["demo-react"], into: IO.stream(:stdio, :line))
+    Process.send_after(self(), :listo, 3000)
     {:noreply, assign(socket, crear_step: "Verificando...", status: "verificar")}
   end
 
